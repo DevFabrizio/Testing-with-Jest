2cdba16da0e90c52c8622ba29d4ced62351f659b
DevFabrizio <>
2023-05-20 13:07:27 +0000

    add boiler plate for simon game

--- script/tests/calc.test.js
+++ script/tests/calc.test.js
@@ -1,9 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
 const addition = require("../calc")
 
 describe('Calculator', () => {
     describe('Addition function', () => {
         test('should return 42 for 20 + 22', () => {
             expect(addition(20,22)).toBe(42)
+        })
+        test('should return 87 for 50 + 37', () => {
+            expect(addition(50,37)).toBe(87)
         })
     })
     describe('Subtraction function', () => {
